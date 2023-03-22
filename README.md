c95d58aeb68ee49c6b31068a9fa3029b4857b7be
Colton Gibson <colton.gibson@exprealty.net>
2023-03-21 18:21:40 -0600

    javascript-dev-test

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# coding-test
+Here we have a product feature that allow users to easily oversee all agents within the company. This is the first iteration into giving our staff memebers the ability to transfer agents directly to outside agents with just a click of a button. As you can see we are having some errors with loading in the list of users as well as actually sorting them. 
+
+Please reference the image attatched below and use this to get a better understanding of what we're kind of looking for in terms of first iteration. Also note this code will fall into the hands of many other engineers so please make it easy to read and follow.
+
+In terms of adding functionality not shown within the screenshot and UI design, please feel free to add what you would like in the allotted time, but do not make it your primary focus. Clean functional code is our main focus here at eXp.
+
+![plot](./frontend/src/images/product_expectation.png)
+
+If you have any questions or concerns feel free to contact Colton Gibson at colton.gibson@exprealty.net or Tyler Bakken @ tyler.bakken@exprealty.net and we will do our best to answer any questions you have as efficiently as we can!
--- frontend/src/components/directory.js
+++ frontend/src/components/directory.js
@@ -10,7 +10,7 @@ import '../App.css'
 
 const Directory = () => {
     const [data, setData] = useState([])
-    const [editAgent, setEditAgent] = useState(false)
+        const [editAgent, setEditAgent] = useState(false)
     const [selectedAgent, setSelectedAgent] = useState({})
     const [updateAgentInfo, setUpdateAgentInfo] = useState({name:'', forwardingNumber:'',  email:'', index:''})
 
@@ -37,15 +37,14 @@ const Directory = () => {
     },[])
 
     const updateAgent = async(ag, index) => {
-        setSelectedAgent(ag)
-        updateAgentInfo.sid = index
-        setEditAgent(!editAgent)
+        setSelectedAgent(ag), selectedAgent.sid = index
+                setEditAgent(!editAgent)
     }
 
     const updateAgentFunc = () => {
-        data[updateAgentInfo.sid].name = updateAgentInfo.name
-        data[updateAgentInfo.sid].email = updateAgentInfo.email
-        data[updateAgentInfo.sid].forwarding_number = updateAgentInfo.forwardingNumber
+        data[selectedAgent.sid].name = updateAgentInfo.name
+            data[selectedAgent.sid].email = updateAgentInfo.email
+        data[selectedAgent.sid].forwarding_number = updateAgentInfo.forwardingNumber
         window.localStorage.setItem("data", JSON.stringify(data))
         setEditAgent(false)
         const newData = localStorage.getItem('data')
@@ -61,24 +60,27 @@ const Directory = () => {
                 <div style={{ overflowX:'hidden', color:'black' }}>
                     <div style={{fontWeight:"Bolder", fontSize:'17px', padding:'40px'}}> Agent Directory</div>
                     <SelectionFilter/>
+                    <div style={{position:'absolute'}}onClick={() => {
+                        setData({})
+                    }}><button>Clear Agents</button></div>
                     {editAgent !== false && (
                         <div className="selectedAgentContainer">
                             <div>
                             Name: 
                             <input className="editAgentInput" value={updateAgentInfo.name} onChange={(e) => {
-                                setUpdateAgentInfo({name:e.target.value, forwardingNumber:updateAgentInfo.forwardingNumber, email:updateAgentInfo.email, sid: updateAgentInfo.sid})
+                                setUpdateAgentInfo({name:e.target.value, forwardingNumber:updateAgentInfo.forwardingNumber, email:updateAgentInfo.email, sid: selectedAgent.sid})
                             }}></input>
                             </div> 
                             <div>
                             Number: 
                             <input className="editAgentInput" value={updateAgentInfo.forwardingNumber} onChange={(e) => {
-                                setUpdateAgentInfo({name:updateAgentInfo.name, forwardingNumber:e.target.value, email:updateAgentInfo.email, sid: updateAgentInfo.sid})
+                                setUpdateAgentInfo({name:updateAgentInfo.name, forwardingNumber:e.target.value, email:updateAgentInfo.email, sid: selectedAgent.sid})
                             }}></input>
                             </div>
                             <div>
                             Email: 
                             <input className="editAgentInput" value={updateAgentInfo.email} onChange={(e) => {
-                                setUpdateAgentInfo({name:updateAgentInfo.name, forwardingNumber:updateAgentInfo.forwardingNumber, email:e.target.value, sid: updateAgentInfo.sid})
+                                setUpdateAgentInfo({name:updateAgentInfo.name, forwardingNumber:updateAgentInfo.forwardingNumber, email:e.target.value, sid: selectedAgent.sid})
                             }}></input>
                             </div>
                             <div>
